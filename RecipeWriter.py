301f7232bd238e7b6812244f2a3622178e6ebe64
Mike Simms <msimms01@gmail.com>
2020-05-07 16:33:45 -0400

    Can now dump recipes to JSON.

--- RecipeWriter.py
+++ RecipeWriter.py
@@ -25,7 +25,9 @@
 import CrawlerDatabase
 import argparse
 import collections
+import json
 
+ID_KEY = '_id'
 STYLE_KEY = 'style'
 YIELD_SIZE_KEY = 'yield size'
 GRAINS_KEY = 'grains'
@@ -35,8 +37,8 @@ FERMENTABLES_KEY = 'Fermentable'
 AMOUNT_KEY = 'Amount'
 VARIETY_KEY = 'Variety'
 BOIL_KEY = 'Boil'
-COMMMON_HOPS = ['cascade', 'kent goldings', 'golding', 'citra', 'centennial', 'mosaic', 'columbus', 'equinox']
-UNITS = ['lb', 'oz', 'tsp', 'pkg', 'ounce', 'teaspoon', 'cup', 'pound', 'gal']
+COMMMON_HOPS = ['cascade', 'centennial', 'citra', 'chinook', 'columbus', 'equinox', 'fuggles', 'kent goldings', 'golding', 'magnum', 'mosaic', 'victory', 'warrior']
+UNITS = ['lb', 'oz', 'g', 'tsp', 'pkg', 'ounce', 'teaspoon', 'cup', 'pound', 'gal']
 
 
 class RecipeWriter(object):
@@ -46,24 +48,39 @@ class RecipeWriter(object):
         super(RecipeWriter, self).__init__()
 
     def normalize_grains_and_hops(self, grains, hops):
+        """Since the recipes were collected from multiple sites, this attempts to normalize their structure."""
+        if grains is None:
+            return grains, hops
+
         new_grains = []
         new_hops = []
 
         search_list_func = lambda x,y : x.find(y) >= 0
-        ignore_strs = ['(', '[', '#', '-', 'sugar', 'water', 'total', 'dme', 'honey', 'syrup', 'moss', 'yeast', 'extract']
+        ignore_strs = ['[', '#', 'dme', 'extract', 'honey', 'moss', 'sugar', 'syrup', 'total', 'water', 'yeast']
 
-        # Normalize grains
+        # Normalize grains, some websites lump the hops in with the grains for whatever reason.
         for grain in grains:
 
             # Was this formatted in a nice is python dictionary for us?
             if isinstance(grain, dict):
+
+                # Do we have a dictionary with valid items?
                 if AMOUNT_KEY in grain and FERMENTABLES_KEY in grain:
+                    
                     amount = grain[AMOUNT_KEY]
                     fermentables = grain[FERMENTABLES_KEY]
+
+                    # Cleanup the name. It might have the amount appended to it.
                     offset = fermentables.find(amount)
                     if offset > 0:
                         grain[FERMENTABLES_KEY] = fermentables[offset + len(amount):].strip()
-                new_grains.append(grain)
+
+                    # Does the string contain junk?
+                    matched_ignore_strs = [search_list_func(grain[FERMENTABLES_KEY].lower(), i) for i in ignore_strs]
+                    if any(matched_ignore_strs):
+                        continue
+
+                    new_grains.append(grain)
             else:
                 # Does the string contain junk?
                 matched_ignore_strs = [search_list_func(grain.lower(), i) for i in ignore_strs]
@@ -80,6 +97,10 @@ class RecipeWriter(object):
                 amount = parts[0]
                 del parts[0]
 
+                # Sanity check.
+                if len(parts) == 0:
+                    continue
+
                 # Is the second part the units?
                 matched_units = [search_list_func(parts[0].lower(), i) for i in UNITS]
                 if any(matched_units):
@@ -88,6 +109,10 @@ class RecipeWriter(object):
                     del parts[0]
                 if '-' in parts:
                     parts.remove('-')
+
+                # Sanity check.
+                if len(parts) == 0:
+                    continue
 
                 # Build the description string and figure out if it is grains or hops
                 # because some websites lump them together.
@@ -98,15 +123,24 @@ class RecipeWriter(object):
 
                     part_lower = part.lower()
 
+                    # Sanity check.
+                    if len(part_lower) == 0:
+                        continue
+
+                    # Some recipes have comments in paraenthesis or brackets at the end of the string. We don't need them.
+                    if part_lower[0] in ['(', '[']:
+                        break
+
+                    # Did someone put hops in the grains category?
                     if part_lower in COMMMON_HOPS:
                         is_grain = False
                         is_hop = True
-
                     elif is_hop:
                         in_boil = in_boil or part_lower.find('at') >= 0 or part_lower.find('boil')
                         if in_boil and part_lower.find('hop') >= 0:
                             boil += part
 
+                    # Fairly sure this is part of the grains.
                     if not in_boil:
                         desc += part
                         desc += " "
@@ -162,6 +196,7 @@ class RecipeWriter(object):
                     hops_value = page[HOPS_KEY]
 
                 norm_grains, norm_hops = self.normalize_grains_and_hops(grain_value, hops_value)
+
                 grains.extend(norm_grains)
                 hops.extend(norm_hops)
 
@@ -196,13 +231,57 @@ class RecipeWriter(object):
         for yeast in counted_yeasts:
             print(yeast)
 
+    def export_to_json(self, db):
+        """Exports the beer recipes to JSON."""
+        all_data = []
+        all_pages = db.retrieve_all_pages()
+        for page in all_pages:
+            grain_value = None
+            hops_value = None
+
+            if GRAINS_KEY in page:
+                grain_value = page[GRAINS_KEY]
+            else: # Filter out pages that aren't recipes
+                continue
+
+            if HOPS_KEY in page:
+                hops_value = page[HOPS_KEY]
+
+            norm_grains, norm_hops = self.normalize_grains_and_hops(grain_value, hops_value)
+
+            page[GRAINS_KEY] = norm_grains
+            page[HOPS_KEY] = norm_hops
+
+            if YEASTS_KEY in page:
+                page[YEASTS_KEY] = self.normalize_yeast(page[YEASTS_KEY])
+
+            # This isn't serializable so get rid of it.
+            if ID_KEY in page:
+                del page[ID_KEY]
+
+            all_data.append(page)
+        
+        return json.dumps(all_data)
+
+    def list_styles(self, db):
+        all_pages = db.retrieve_all_pages()
+
+        # The recipes were collected from various sites and will need normalizing
+        # after we filter for the recipes that match the search criteria.
+        for page in all_pages:
+
+            if STYLE_KEY in page:
+                pass
+
 def main():
     """This is the entry point that is used to perform unit tests on this module."""
 
     # Command line options.
     parser = argparse.ArgumentParser()
-    parser.add_argument("--style", default="", help="Style of beers to dump.", required=False)
+    parser.add_argument("--style", default=None, help="Style of beers to dump.", required=False)
     parser.add_argument("--mongodb-addr", default="localhost:27017", help="Address of the mongo database.", required=False)
+    parser.add_argument("--json", action="store_true", default=False, help="Exports the recipes as JSON.", required=False)
+    parser.add_argument("--list-styles", action="store_true", default=False, help="Prints all styles of beer found in the database.", required=False)
     args = parser.parse_args()
 
     # Instantiate the object that connects to the database.
@@ -211,15 +290,30 @@ def main():
         db = CrawlerDatabase.MongoDatabase()
         db.connect(args.mongodb_addr)
 
+    # Sanity check.
+    if db is None:
+        print("ERROR: No database.")
+
     # This option allows the user to dump recipes to stdout.
     if args.style is not None:
 
-        # Sanity check.
-        if db is None:
-            print("ERROR: No database.")
-
         writer = RecipeWriter()
         writer.generate_avg_recipe(db, args.style)
+
+    # Are we exporting the recipes?
+    if args.json:
+
+        writer = RecipeWriter()
+        data = writer.export_to_json(db)
+        print(data)
+
+    # Are we exporting the styles?
+    if args.list_styles:
+
+        writer = RecipeWriter()
+        data = writer.list_styles(db)
+        print(data)
+
 
 if __name__ == "__main__":
     main()
